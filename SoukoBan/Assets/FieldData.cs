09bf75a1ab37e58a235e1b2c9d79c7f99703e4fe
STD\s20217022 <20217022-YoshidaYugo@hcs.ac.jp>
2023-01-31 15:13:58 +0900

    コードレビュー後の修正プロジェクトです

--- SoukoBan/Assets/FieldData.cs
+++ SoukoBan/Assets/FieldData.cs
@@ -2,99 +2,98 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// ---------------------------------------------------------
+// FieldData.cs
+//
+// 作成者:吉田雄伍
+// 入力されたデータ通りにフィールドを生成するスクリプト
+// ---------------------------------------------------------
+
 public class FieldData : MonoBehaviour
 {
     [SerializeField, Header("ブロックマス(壁)のオブジェクト")]
     private GameObject _blockSquaresObj;
     [SerializeField, Header("空のマスのオブジェクト")]
-    private GameObject _nullSquaresObj;
+    private GameObject _emptySquaresObj;
 
-    [Header("値の入力時は必ずブロックマスからスタートする")]
+    [Header("値の入力時は必ずブロックマスからスタートする。 ※上下反転します")]
     [SerializeField, TextArea, Header("(2,5,1) ブロックマス(壁),通れるマス,ブロックマス")]
     private string _inputFieldData;
 
-    //フィールドの列数と行数
+    // フィールドの列数と行数
     private int _fieldColumn = default;
     private int _fieldRow = default;
 
-    //フィールドデータを格納している配列
+    // フィールドデータを格納している配列
     public int[,] _fieldDataArray { get; private set; }
 
-    //ブロックが入っていないマス(通れるマス)を0、入っている場所を1とする
-    private const int _storeBlockNumber = 1;
-    private const int _nullSquaresNumber = 0;
+    // ブロックが入っていないマス(通れるマス)を0、入っている場所を1とする
+    private const int STORE_BLOCK_NUMBER = 1;
+    private const int EMPTY_BLOCK_NUMBER = 0;
 
 
     /// <summary>
-    ///入力されたテキストに応じて、マップ生成を行う 
+    /// 入力されたテキストに応じて、マップ生成を行う 
     /// </summary>
     private void Awake()
     {
+        // フィールドデータの配列の大きさを定義する
         GetFieldDataArraySize();
 
-        //フィールドデータを抽出する変数
+        // フィールドデータを抽出する変数
         string inputFieldDataValue = default;
 
-        //データ格納中の行
+        // データ格納中の行
         int blockSetRow = 0;
 
-        //マスに値をセットする際の行中のスタート位置
+        // マスに値をセットする際の行中のスタート位置
         int blockSetStartColumn = 0;
 
-        //格納するマスに値を入れる変数とそれに対応する生成オブジェクト
+        // 格納するマスに値を入れる変数とそれに対応する生成オブジェクト
         int squareValue = default;
         GameObject instantObj = default;
 
-        //フィールドデータを格納する処理
+        // フィールドデータを格納する処理
         for (int i = 0; i < _inputFieldData.Length; i++)
         {
-            //数値をひとつ記録する処理
+            // 入力されたデータ中の数値を一つずつ抽出する処理
             for (int m = i; i < _inputFieldData.Length && _inputFieldData[m] != ',' && _inputFieldData[m] != '\n'; m++)
             {
-                //テキストで入力された数値を記録する
+                // 抽出した数値を格納する
                 inputFieldDataValue = inputFieldDataValue + _inputFieldData[m].ToString();
                 i++;
             }
 
-            //抽出されたデータを値型に変換する処理
+            // 抽出されたデータを値型に変換する処理
             int blockSetNumber = int.Parse(inputFieldDataValue);
             inputFieldDataValue = default;
 
-            //格納を始める列位置が0ならブロックマスの値を入れる(行の最初がブロックマスであるため)
-            if (blockSetStartColumn == 0)
+            // 格納する列位置が０(入力データの最初がブロックマス)または、前回格納した値が空マスならブロックマスの値を入れる
+            if (squareValue == EMPTY_BLOCK_NUMBER || blockSetStartColumn == 0)
             {
-                squareValue = _storeBlockNumber;
+                squareValue = STORE_BLOCK_NUMBER;
                 instantObj = _blockSquaresObj;
             }
+            // 前回格納した値がブロックマスなら、空マスの値を入れる
             else
             {
-                //前回格納した値がブロックマスなら、空マスの値を入れる
-                if (squareValue == _storeBlockNumber)
-                {
-                    squareValue = _nullSquaresNumber;
-                    instantObj = _nullSquaresObj;
-                }
-                //前回格納した値が空マスなら、ブロックマスの値を入れる
-                else
-                {
-                    squareValue = _storeBlockNumber;
-                    instantObj = _blockSquaresObj;
-                }
+                squareValue = EMPTY_BLOCK_NUMBER;
+                instantObj = _emptySquaresObj;
             }
 
-            //配列に格納し、オブジェクトを配置する処理
+            // 配列に格納し、オブジェクトを配置する処理
             for (int k = 0; k < blockSetNumber; k++)
             {
-                //配列(_fieldDataArray)に値をセットする
+                // 配列(_fieldDataArray)に値をセットする
                 _fieldDataArray[blockSetStartColumn + k, blockSetRow] = squareValue;
-                //フィールド上にオブジェクトを配置する
+                // フィールド上にオブジェクトを配置する
                 Instantiate(instantObj).transform.position = new Vector3(blockSetStartColumn + k, blockSetRow, 0);
             }
 
-            //格納を始める列位置を更新
+            // 格納を始める列位置を更新
             blockSetStartColumn += blockSetNumber;
 
-            //列数と格納を始める列位置が一致したら、次の行に更新する
+            // 列数と格納を始める列位置が一致したら、次の行に更新する
             if (blockSetStartColumn == _fieldColumn)
             {
                 blockSetRow += 1;
@@ -109,31 +108,31 @@ public class FieldData : MonoBehaviour
     /// </summary>
     private void GetFieldDataArraySize()
     {
-        //フィールドデータから列数を抽出するための変数
+        // フィールドデータから列数を抽出するための変数
         string FieldDataCulumnValue = default;
 
-        //フィールドの列数を取得する(すべてのマスにブロックマスが入るの一行目から抽出)
+        // フィールドの列数を取得する(すべてのマスにブロックマスが入るの一行目から抽出)
         for (int i = 0; _inputFieldData[i] != ',' && _inputFieldData[i] != '\n'; i++)
         {
-            //列数を記録する
+            // 列数を記録する
             FieldDataCulumnValue += _inputFieldData[i].ToString();
         }
-        //列数を数値化する
+        // 列数を数値化する
         _fieldColumn += int.Parse(FieldDataCulumnValue);
         FieldDataCulumnValue = default;
 
 
-        //フィールドの行数を取得する
+        // フィールドの行数を取得する
         _fieldRow = 1;
         for (int i = 0; i < _inputFieldData.Length; i++)
         {
-            //改行があったら行数をプラスする
+            // 改行があったら行数をプラスする
             if (_inputFieldData[i] == '\n')
             {
                 _fieldRow += 1;
             }
         }
-        //フィールドデータ(二次元配列)の大きさを定義する
+        // フィールドデータ(二次元配列)の大きさを定義する
         _fieldDataArray = new int[_fieldColumn, _fieldRow];
 
     }
--- SoukoBan/Assets/PlayerBoxControl.cs
+++ SoukoBan/Assets/PlayerBoxControl.cs
@@ -3,22 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+// ---------------------------------------------------------
+// FieldData.cs
+//
+// 作成者:吉田雄伍
+// プレイヤーのコントローラ入力に応じた操作を行うスクリプト
+// ---------------------------------------------------------
+
 public class PlayerBoxControl : MonoBehaviour
 {
-    [SerializeField]
+    [SerializeField, Tooltip("フィールドのデータのスクリプト")]
     private FieldData _fieldData;
 
     [SerializeField, Header("プレイヤーのオブジェクト")]
     private GameObject _playerObj;
 
     [SerializeField, Header("プレイヤーの初期位置")]
-    private Vector2 _playerInisialPos;
+    private Vector2 _playerInitialPos;
+
 
     [SerializeField, Header("箱のオブジェクト")]
     private GameObject _boxObj;
 
     [SerializeField, Header("箱の初期位置")]
-    private Vector2[] _boxInisialPos;
+    private Vector2[] _boxInitialPos;
 
     [SerializeField, Header("箱のゴール位置のオブジェクト")]
     private GameObject _boxGoalObj;
@@ -33,43 +41,49 @@ public class PlayerBoxControl : MonoBehaviour
     [SerializeField, Header("このScene名")]
     private string _thisSceneName;
 
-    //ブロックが入っていないマス(通れるマス)を0、入っている場所を1、箱が入っているマスを2とする
-    private const int _storeBlockNumber = 1;
-    private const int _nullSquaresNumber = 0;
-    private const int _storeBoxNumber = 2;
+    // ブロックが入っていないマス(通れるマス)を0、入っている場所を1、箱が入っているマスを2とする
+    private const int STORE_BLOCK_NUMBER = 1;
+    private const int EMPTY_BLOCK_NUMBER = 0;
+    private const int STORE_BOX_NUMBER = 2;
 
 
-    //箱のオブジェクトと位置情報を格納している変数
-    private GameObject[] _boxObjects;
-    private int[] _boxCurrentPosX;
-    private int[] _boxCurrentPosY;
+    // 箱のオブジェクトと位置情報を格納している変数
+    private GameObject[] _boxObjects = default;
+    private int[] _boxCurrentPosX = default;
+    private int[] _boxCurrentPosY = default;
 
-    //箱がゴールしているかを判定するフラグ
-    private bool[] _isBoxGoal;
+    // 箱がゴールしているかを判定するフラグ
+    private bool[] _isBoxGoal = default;
 
-    //プレイヤーの現在位置情報
-    private int _playerCurrentPosX;
-    private int _playerCurrentPosY;
+    // プレイヤーの現在位置情報
+    private int _playerCurrentPosX = default;
+    private int _playerCurrentPosY = default;
 
-    //フィールドデータを格納する変数
-    private int[,] _fieldDataArray;
+    // フィールドデータを格納する変数
+    private int[,] _fieldDataArray = default;
 
 
-    //プレイヤーの移動先予定の位置
-    private int playerDestinationPosX　= default;
-    private int playerDestinationPosY　= default;
+    // プレイヤーの移動先予定の位置
+    private int _playerDestinationPosX = default;
+    private int _playerDestinationPosY = default;
 
-    //移動先に箱があった場合の箱の移動先予定の位置
-    private int boxDestinationPosX = default;
-    private int boxDestinationPosY = default;
+    // 移動先に箱があった場合の箱の移動先予定の位置
+    private int _boxDestinationPosX = default;
+    private int _boxDestinationPosY = default;
+
+    private float _beforeVerticalInput = default;
+    private float _beforeHorizontalInput = default;
+
 
     private void Start()
     {
-        //ブロックが入っていないマス(通れるマス)を0、入っている場所を1、箱があるマスを2
+        // ブロックが入っていないマス(通れるマス)を0、入っている場所を1、箱があるマスを2
         _fieldDataArray = _fieldData._fieldDataArray;
 
+        // プレイヤーを初期位置に移動する
         PlayerStart();
 
+        // 箱とゴールを生成する
         BoxStart();
     }
 
@@ -79,10 +93,10 @@ public class PlayerBoxControl : MonoBehaviour
     /// </summary>
     private void PlayerStart()
     {
-        //プレイヤーを初期位置に移動する
-        _playerCurrentPosX = (int)_playerInisialPos.x;
-        _playerCurrentPosY = (int)_playerInisialPos.y;
-        _playerObj.transform.position = _playerInisialPos;
+        // プレイヤーを初期位置に移動する
+        _playerCurrentPosX = (int)_playerInitialPos.x;
+        _playerCurrentPosY = (int)_playerInitialPos.y;
+        _playerObj.transform.position = _playerInitialPos;
     }
 
 
@@ -91,41 +105,45 @@ public class PlayerBoxControl : MonoBehaviour
     /// </summary>
     private void BoxStart()
     {
-        //配列の大きさを定義
-        _boxObjects = new GameObject[_boxInisialPos.Length];
-        _boxCurrentPosX = new int[_boxInisialPos.Length];
-        _boxCurrentPosY = new int[_boxInisialPos.Length];
+        // 配列の大きさを定義
+        _boxObjects = new GameObject[_boxInitialPos.Length];
+        _boxCurrentPosX = new int[_boxInitialPos.Length];
+        _boxCurrentPosY = new int[_boxInitialPos.Length];
 
         _isBoxGoal = new bool[_boxGoalPos.Length];
 
 
-        //箱の初期位置の設定をする処理
-        for (int i = 0; i < _boxInisialPos.Length; i++)
+        // 箱の初期位置の設定をする処理
+        for (int i = 0; i < _boxInitialPos.Length; i++)
         {
-            //箱のゴール地点を生成する
+            // 箱のゴール地点を生成する
             Instantiate(_boxGoalObj).transform.position = _boxGoalPos[i];
             
-            //箱の初期位置を箱の現在地に格納する
-            _boxCurrentPosX[i] = (int)_boxInisialPos[i].x;
-            _boxCurrentPosY[i] = (int)_boxInisialPos[i].y;
+            // 箱の初期位置を箱の現在地に格納する
+            _boxCurrentPosX[i] = (int)_boxInitialPos[i].x;
+            _boxCurrentPosY[i] = (int)_boxInitialPos[i].y;
 
-            //フィールド情報に箱の値を設定する
-            _fieldDataArray[(int)_boxInisialPos[i].x, (int)_boxInisialPos[i].y] = _storeBoxNumber;
+            // フィールド情報に箱の値を設定する
+            _fieldDataArray[(int)_boxInitialPos[i].x, (int)_boxInitialPos[i].y] = STORE_BOX_NUMBER;
 
-            //箱を初期位置に生成する
+            // 箱を初期位置に生成する
             _boxObjects[i] = Instantiate(_boxObj);
-            _boxObjects[i].transform.position = _boxInisialPos[i];
+            _boxObjects[i].transform.position = _boxInitialPos[i];
 
-            //動かした箱がゴールに入ったかどうかを検知する(jはゴールの数)
+            // 動かした箱がゴールに入ったかどうかを検知する(jはゴールの数)
             for (int j = 0; j < _boxGoalPos.Length; j++)
             {
-                //箱の現在地を格納する
+                // 箱の現在地を格納する
                 Vector2 boxCurrentPos = new Vector2(_boxCurrentPosX[i], _boxCurrentPosY[i]);
 
-                //箱とゴール地点が一致したらすべてゴールしているか
+                // 箱とゴールの位置が一致しているか
                 if (_boxGoalPos[j] == boxCurrentPos)
                 {
+                    // ゴールフラグをセットする
                     _isBoxGoal[i] = true;
+
+                    // 全ての箱がゴールしているかをチェックする
+                    BoxGoalCheck();
                 }
             }
         }
@@ -133,65 +151,120 @@ public class PlayerBoxControl : MonoBehaviour
 
     private void Update()
     {
-        //プレイヤーの移動先予定の位置
-        playerDestinationPosX = _playerCurrentPosX;
-        playerDestinationPosY = _playerCurrentPosY;
+        // プレイヤーの移動先予定の位置
+        _playerDestinationPosX = _playerCurrentPosX;
+        _playerDestinationPosY = _playerCurrentPosY;
 
-        //移動先に箱があった場合の箱の移動先予定の位置
-        boxDestinationPosX = _playerCurrentPosX;
-        boxDestinationPosY = _playerCurrentPosY;
+        // 移動先に箱があった場合の箱の移動先予定の位置
+        _boxDestinationPosX = _playerCurrentPosX;
+        _boxDestinationPosY = _playerCurrentPosY;
 
-        ///ボタンを押したときの処理///
-        //一回の入力あたりの移動量
-        int _oneInputMoveMass = 1;
+        /// ボタンを押したときの処理 ///
+        
+        // 入力に応じて移動処理を行う処理
+        PlayerVerticalInputProcess();
+        PlayerHorizontalInputProcess();
 
-        //上に移動
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            //プレイヤーの移動予定位置と箱の移動予定位置を格納する
-            playerDestinationPosY = _playerCurrentPosY + _oneInputMoveMass;
-            boxDestinationPosY = playerDestinationPosY + _oneInputMoveMass;
-
-            MoveProcessing();
-            return;
-        }
-        //下に移動
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            playerDestinationPosY = _playerCurrentPosY - _oneInputMoveMass;
-            boxDestinationPosY = playerDestinationPosY - _oneInputMoveMass;
-
-            MoveProcessing();
-            return;
-        }
-        //右に移動
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            playerDestinationPosX = _playerCurrentPosX + _oneInputMoveMass;
-            boxDestinationPosX = playerDestinationPosX + _oneInputMoveMass;
-
-            MoveProcessing();
-            return;
-        }
-        //左に移動
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            playerDestinationPosX = _playerCurrentPosX - _oneInputMoveMass;
-            boxDestinationPosX = playerDestinationPosX - _oneInputMoveMass;
-
-            MoveProcessing();
-            return;
-        }
-
-        //リスタート
+        // リスタート
         if (Input.GetKeyDown(KeyCode.R))
         {
             SceneManager.LoadScene(_thisSceneName);
         }
-        //ゲームの終了
+        // ゲームの終了
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             Application.Quit();
+        }
+    }
+
+
+    /// <summary>
+    /// 縦軸の入力があった時の処理
+    /// </summary>
+    private void PlayerVerticalInputProcess()
+    {
+        // 一回の入力あたりの移動量
+        int oneInputMoveMass = 1;
+
+        // 縦軸の入力値
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // 前フレームに縦軸の入力をしていた場合、移動をしない
+        if (_beforeVerticalInput != 0)
+        {
+            // 現在フレームの入力値を格納する
+            _beforeVerticalInput = verticalInput;
+
+            return;
+        }
+
+        // 現在フレームの入力値を格納する
+        _beforeVerticalInput = verticalInput;
+
+        // 上に移動
+        if (verticalInput > 0)
+        {
+            // プレイヤーの移動予定位置と箱の移動予定位置を格納する
+            _playerDestinationPosY = _playerCurrentPosY + oneInputMoveMass;
+            _boxDestinationPosY = _playerDestinationPosY + oneInputMoveMass;
+
+            // プレイヤーと箱の移動処理を行う
+            MoveProcessing();
+            return;
+        }
+        // 下に移動
+        if (verticalInput < 0)
+        {
+            _playerDestinationPosY = _playerCurrentPosY - oneInputMoveMass;
+            _boxDestinationPosY = _playerDestinationPosY - oneInputMoveMass;
+
+            MoveProcessing();
+            return;
+        }
+    }
+
+
+    /// <summary>
+    /// 横軸の入力があった時の処理
+    /// </summary>
+    private void PlayerHorizontalInputProcess()
+    {
+        // 一回の入力あたりの移動量
+        int oneInputMoveMass = 1;
+
+        // コントローラとキーの入力量
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        // 前フレームに横軸の入力をしていた場合、移動をしない
+        if (_beforeHorizontalInput != 0)
+        {
+            // 現在フレームの入力値を格納する
+            _beforeHorizontalInput = horizontalInput;
+            return;
+        }
+
+        // 現在フレームの入力値を格納する
+        _beforeHorizontalInput = horizontalInput;
+
+        // 右に移動
+        if (horizontalInput > 0)
+        {
+            // プレイヤーの移動予定位置と箱の移動予定位置を格納する
+            _playerDestinationPosX = _playerCurrentPosX + oneInputMoveMass;
+            _boxDestinationPosX = _playerDestinationPosX + oneInputMoveMass;
+
+            // プレイヤーと箱の移動処理を行う
+            MoveProcessing();
+            return;
+        }
+        // 左に移動
+        if (horizontalInput < 0)
+        {
+            _playerDestinationPosX = _playerCurrentPosX - oneInputMoveMass;
+            _boxDestinationPosX = _playerDestinationPosX - oneInputMoveMass;
+
+            MoveProcessing();
+            return;
         }
     }
 
@@ -201,62 +274,66 @@ public class PlayerBoxControl : MonoBehaviour
     /// </summary>
     private void MoveProcessing()
     {
-        //移動先が空のマスの時の処理
-        if (_fieldDataArray[playerDestinationPosX, playerDestinationPosY] == _nullSquaresNumber)
+        // 移動先が空のマスの時の処理
+        if (_fieldDataArray[_playerDestinationPosX, _playerDestinationPosY] == EMPTY_BLOCK_NUMBER)
         {
+            // プレイヤーを移動する処理
             PlayerMove();
 
             return;
         }
 
-        //移動先に箱がないときは処理しない
-        if (_fieldDataArray[playerDestinationPosX, playerDestinationPosY] != _storeBoxNumber)
+        // 移動先に箱がないときは処理しない
+        if (_fieldDataArray[_playerDestinationPosX, _playerDestinationPosY] != STORE_BOX_NUMBER)
         {
             return;
         }
-        //箱が移動できないときは処理しない
-        else if(_fieldDataArray[boxDestinationPosX, boxDestinationPosY] != _nullSquaresNumber)
+        // 箱が移動できないときは処理しない
+        else if(_fieldDataArray[_boxDestinationPosX, _boxDestinationPosY] != EMPTY_BLOCK_NUMBER)
         {
             return;
         }
 
-        //箱が移動できるときの処理
+        // 箱が移動できるときの処理
         for (int i = 0; i < _boxObjects.Length; i++)
         {
-            //プレイヤーが押し出す箱を検索し、その箱を動かす
-            if (playerDestinationPosX == _boxCurrentPosX[i] && playerDestinationPosY == _boxCurrentPosY[i])
+            // プレイヤーが押し出す箱を検索し、その箱を動かす
+            if (_playerDestinationPosX == _boxCurrentPosX[i] && _playerDestinationPosY == _boxCurrentPosY[i])
             {
-                //フィールド情報の値を変更する
-                _fieldDataArray[boxDestinationPosX, boxDestinationPosY] = _storeBoxNumber;
-                _fieldDataArray[_boxCurrentPosX[i], _boxCurrentPosY[i]] = _nullSquaresNumber;
+                // フィールド情報の値を変更する
+                _fieldDataArray[_boxDestinationPosX, _boxDestinationPosY] = STORE_BOX_NUMBER;
+                _fieldDataArray[_boxCurrentPosX[i], _boxCurrentPosY[i]] = EMPTY_BLOCK_NUMBER;
 
-                //箱を動かす
-                _boxObjects[i].transform.position = new Vector2(boxDestinationPosX, boxDestinationPosY);
+                // 箱を動かす
+                _boxObjects[i].transform.position = new Vector2(_boxDestinationPosX, _boxDestinationPosY);
 
-                //箱の現在地を更新
-                _boxCurrentPosX[i] = boxDestinationPosX;
-                _boxCurrentPosY[i] = boxDestinationPosY;
+                // 箱の現在地を更新
+                _boxCurrentPosX[i] = _boxDestinationPosX;
+                _boxCurrentPosY[i] = _boxDestinationPosY;
 
+                // プレイヤーを移動する処理
                 PlayerMove();
 
-                //動かした箱がゴールに入ったかどうかを検知する(jはゴールの数)
+                // 動かした箱がゴールに入ったかどうかを検知する(jはゴールの数)
                 for (int j = 0; j < _boxGoalPos.Length; j++)
                 {
-                    //箱の現在地を格納する
+                    // 箱の現在地を格納する
                     Vector2 boxCurrentPos = new Vector2(_boxCurrentPosX[i], _boxCurrentPosY[i]);
                 
-                    //動かした箱がゴールした時の処理
+                    // 動かした箱がゴールした時の処理
                     if (_boxGoalPos[j] == boxCurrentPos)
                     {
-                        //ゴールフラグをセットする
+                        // ゴールフラグをセットする
                         _isBoxGoal[i] = true;
 
+                        // 全ての箱がゴールしているかをチェックする
                         BoxGoalCheck();
+
                         return;
                     }
                 }
 
-                //ゴールしていなかった時の処理
+                // ゴールしていなかった時の処理
                 _isBoxGoal[i] = false;
             }
         }
@@ -268,12 +345,12 @@ public class PlayerBoxControl : MonoBehaviour
     /// </summary>
     private void PlayerMove()
     {
-        //プレイヤーを移動する
-        _playerObj.transform.position = new Vector2(playerDestinationPosX, playerDestinationPosY);
+        // プレイヤーを移動する
+        _playerObj.transform.position = new Vector2(_playerDestinationPosX, _playerDestinationPosY);
 
-        //プレイヤーの現在地を更新
-        _playerCurrentPosX = playerDestinationPosX;
-        _playerCurrentPosY = playerDestinationPosY;
+        // プレイヤーの現在地を更新
+        _playerCurrentPosX = _playerDestinationPosX;
+        _playerCurrentPosY = _playerDestinationPosY;
     }
 
 
@@ -282,6 +359,7 @@ public class PlayerBoxControl : MonoBehaviour
     /// </summary>
     private void BoxGoalCheck()
     {
+        // すべてゴールしているかを判定する
         for(int i = 0; i < _isBoxGoal.Length; i++)
         {
             if(!_isBoxGoal[i])
@@ -290,7 +368,7 @@ public class PlayerBoxControl : MonoBehaviour
             }
         }
 
-        //ゴール処理
+        // ゴール処理
         _goalTextObj.SetActive(true);
     }
 }
